af938e3ab3804204cb0d3f64051ab87ef76852c3
CONG CHEN <67816@supinfo.com>
2019-01-22 23:14:02 +0100

    add new RobotPart

---
+++ src/main/java/com/chen/exercise2/Shoulder.java
@@ -0,0 +1,28 @@
+package com.chen.exercise2;
+
+import com.chen.exercise2.RobotStatus;
+
+/**
+ * Created by: ccong
+ * Date: 19/1/21 下午11:18
+ */
+public class Shoulder extends RobotPartAbstract {
+    private int maxCharge = 100;
+    private RobotStatus robotStatus = RobotStatus.OFF;
+
+    public void setRobotStatus(RobotStatus robotStatus) {
+        this.robotStatus = robotStatus;
+    }
+
+    public void doAction() {
+        System.out.println("Hip doAction");
+    }
+
+    public void doReport() {
+        System.out.println(this.robotStatus);
+    }
+
+    public double calculateTotalCost() {
+        return 0.2*maxCharge/1000;
+    }
+}
